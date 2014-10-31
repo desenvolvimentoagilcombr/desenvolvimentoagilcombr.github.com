652fc41d15313e3fb5dfa40bf81ebedd10c6104e
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2014-10-31 13:57:01 -0200

    Created aside script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 $(function(){
-  $("#content-table-link").on("click", function(e){
+  $("#content-sidebar h4").on("click", function(e){
     e.preventDefault();
-    $("#content-table").slideToggle();
+    $(this).next(".topic ul").toggle();
   });
 
   dp.SyntaxHighlighter.BloggerMode();
