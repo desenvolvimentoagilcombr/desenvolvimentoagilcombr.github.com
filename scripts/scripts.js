947131c73535f5c0f6a050d6bf6b35e81888d2f2
marcioc0sta <marciocst2@gmail.com>
2014-10-31 16:34:08 -0200

    add xp sidemenu to mobile

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,3 +7,17 @@ $(function(){
   dp.SyntaxHighlighter.BloggerMode();
   dp.SyntaxHighlighter.HighlightAll('code');
 })
+
+$(function(){
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
